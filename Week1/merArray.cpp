3edfb3a3412ed9d7b50e358e08009ea783a19c9c
KhaThai <williamalbus26@gmail.com>
2026-03-11 23:51:16 +0700

    first algorithm commit

---
+++ Week1/merArray.cpp
@@ -0,0 +1,47 @@
+#include <vector>
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+
+
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
+
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j]) {
+                nums1[k] = nums1[i];
+
+                i--;
+
+            } else {
+                nums1[k] = nums2[j];
+                j--;
+            }
+            k--;
+        }
+
+        while (j >= 0) {
+            nums1[k] = nums2[j];
+            j--;
+            k--;
+        }
+    }
+};
+
+int main() {
+
+    vector<int> a = {1,2,3,0,0,0};
+    vector<int> b = {2,5,6};
+
+    Solution s;
+    s.merge(a,3,b,3);
+
+    for(int x : a)
+        cout << x << " ";
+}
